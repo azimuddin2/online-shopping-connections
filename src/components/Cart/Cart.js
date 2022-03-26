881e9ca7dbb1ffb89fac2014aa99ecfa0cbe9545
azimuddin2 <mohammadazimuddin274@gmail.com>
2022-03-26 20:13:26 +0600

    readme file update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,13 +4,28 @@ import './Cart.css'
 
 const Cart = (props) => {
     const {cart} = props;
+
+    const chooseToCartWatch = () => {
+        // const chooseValue = Math.floor(Math.random() * 9);
+        for (const product of cart) {
+
+
+            console.log(product.id);
+            if (parseInt(product.id) === Math.floor(Math.random() * 12)) {
+                alert(product.name);
+            }
+        }
+    }
+
     return (
         <div className='cart'>
             <h1 className='selected-title'>Selected Watch</h1>
             {
                 cart.map(cart => <Select  cart={cart}></Select>)
             }
-            <button className='choose-one'>Choose One For Me</button>
+            <button onClick={chooseToCartWatch} className='choose-one'>
+            Choose One For Me
+            </button>
             <button className='choose-again'>Choose Again</button>
         </div>
     );
